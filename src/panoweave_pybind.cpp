59ded7874b76c422ca84dcde21a2b9a5c64a2563
Niklas Digakis <niklas.digakis@studmail.w-hs.de>
2025-06-02 15:07:20 +0000

    Add Python Bindings

---
+++ src/panoweave_pybind.cpp
@@ -0,0 +1,175 @@
+#include "panoweave.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+namespace PanoWeave
+{
+    template <typename T>
+    using ndarray = pybind11::array_t<T, pybind11::array::c_style | pybind11::array::forcecast>;
+    using PyCvMat = ndarray<uint8_t>;
+
+    class PyPanoWeave : public PanoWeave
+    {
+    public:
+        PyPanoWeave() : PanoWeave() {}
+        PyPanoWeave(const std::string &calibration_filepath)
+            : PanoWeave(calibration_filepath) {}
+        PyPanoWeave(const std::string &calibration_filepath, ScalarT depth)
+            : PanoWeave(calibration_filepath, depth) {}
+        PyPanoWeave(const std::string &calibration_filepath, PyCvMat depth)
+            : PanoWeave(calibration_filepath, cv::Mat(depth.shape(0), depth.shape(1), CvMatT(1), depth.mutable_data())) {}
+
+        void setDepth(PyCvMat depth)
+        {
+            PanoWeave::setDepth(cv::Mat(depth.shape(0), depth.shape(1), CvMatT(1), depth.mutable_data()));
+        }
+
+        std::tuple<int, int> resolution() const
+        {
+            return std::make_tuple(this->width(), this->height());
+        }
+        std::tuple<int, int> resolution(const std::tuple<int, int> &resolution)
+        {
+            PanoWeave::resolution(std::get<0>(resolution), std::get<1>(resolution));
+            return resolution;
+        }
+        std::tuple<int, int> resolution(int width, int height)
+        {
+            PanoWeave::resolution(width, height);
+            return this->resolution();
+        }
+
+        std::tuple<ScalarT, ScalarT> fov() const
+        {
+            return std::make_tuple(this->fovX(), this->fovY());
+        }
+        std::tuple<ScalarT, ScalarT> fov(ScalarT fov_x, ScalarT fov_y)
+        {
+            PanoWeave::fov(fov_x, fov_y);
+            return this->fov();
+        }
+        std::tuple<ScalarT, ScalarT> fov(const std::tuple<ScalarT, ScalarT> &fov)
+        {
+            PanoWeave::fov(std::get<0>(fov), std::get<1>(fov));
+            return fov;
+        }
+
+        ndarray<ScalarT> transform() const
+        {
+            ndarray<ScalarT> tf({4UL, 4UL});
+            std::memcpy(tf.mutable_data(), PanoWeave::transform().matrix.val, sizeof(ScalarT) * 16);
+            return tf;
+        }
+        ndarray<ScalarT> transform(const ndarray<ScalarT> &matrix,
+                                   const std::optional<std::tuple<ScalarT, ScalarT, ScalarT>> &translation)
+        {
+            if (matrix.shape(0) == 3 && matrix.shape(1) == 3)
+            {
+                CvAffine3T::Mat3 rmat;
+                std::memcpy(rmat.val, matrix.data(), sizeof(ScalarT) * 9);
+                if (translation.has_value())
+                    PanoWeave::transform(rmat, CvAffine3T::Vec3(std::get<0>(*translation), std::get<1>(*translation), std::get<2>(*translation)));
+                else
+                    PanoWeave::transform(rmat);
+            }
+            else if (matrix.shape(0) == 3 && matrix.shape(1) == 4)
+            {
+                CvAffine3T::Mat3 rot_mat;
+                for (uint i = 0; i < 3; ++i)
+                    std::memcpy(rot_mat.val + 3 * i, matrix.data() + 3 * i, sizeof(ScalarT) * 3);
+                PanoWeave::transform(rot_mat, CvAffine3T::Vec3(matrix.at(0, 3), matrix.at(1, 3), matrix.at(2, 3)));
+            }
+            else if (matrix.shape(0) == 4 && matrix.shape(1) == 4)
+            {
+                CvAffine3T::Mat4 affine;
+                std::memcpy(affine.val, matrix.data(), sizeof(ScalarT) * 16);
+                PanoWeave::transform(affine);
+            }
+            else
+                throw std::runtime_error("Invalid matrix shape for transform. Expected 3x3, 3x4, or 4x4 matrix.");
+            return this->transform();
+        }
+        ndarray<ScalarT> transform(const std::tuple<ScalarT, ScalarT, ScalarT> &rotation,
+                                   const std::optional<std::tuple<ScalarT, ScalarT, ScalarT>> &translation)
+        {
+            if (translation.has_value())
+                PanoWeave::transform(CvAffine3T::Vec3(std::get<0>(rotation), std::get<1>(rotation), std::get<2>(rotation)),
+                                     CvAffine3T::Vec3(std::get<0>(*translation), std::get<1>(*translation), std::get<2>(*translation)));
+            else
+                PanoWeave::transform(CvAffine3T::Vec3(std::get<0>(rotation), std::get<1>(rotation), std::get<2>(rotation)));
+            return this->transform();
+        }
+
+        PyCvMat weave(std::vector<PyCvMat> &images)
+        {
+            std::vector<cv::Mat> cv_images;
+            cv_images.reserve(images.size());
+            for (auto &img : images)
+            {
+                cv_images.emplace_back(img.shape(0), img.shape(1), CV_8UC(img.shape(2)), img.mutable_data());
+            }
+
+            auto pano = PyCvMat({static_cast<size_t>(PanoWeave::height()), static_cast<size_t>(PanoWeave::width()), static_cast<size_t>(images[0].shape(2))});
+            auto cv_pano = cv::Mat(pano.shape(0), pano.shape(1), CV_8UC(pano.shape(2)), pano.mutable_data());
+
+            PanoWeave::weave(cv_images, cv_pano);
+            return pano;
+        }
+        PyCvMat weave(std::vector<PyCvMat> &images, ScalarT depth)
+        {
+            PanoWeave::setDepth(depth);
+            return this->weave(images);
+        }
+        PyCvMat weave(std::vector<PyCvMat> &images, PyCvMat depth)
+        {
+            this->setDepth(depth);
+            return this->weave(images);
+        }
+    };
+
+    PYBIND11_MODULE(panoweave, m)
+    {
+        m.doc() = "PanoWeave Python Bindings";
+
+        pybind11::class_<PyPanoWeave>(m, "Stitcher")
+            .def(pybind11::init<>())
+            .def(pybind11::init<const std::string &>(), pybind11::arg("calibration_filepath"))
+            .def(pybind11::init<const std::string &, ScalarT>(), pybind11::arg("calibration_filepath"), pybind11::arg("depth"))
+            .def(pybind11::init<const std::string &, PyCvMat>(), pybind11::arg("calibration_filepath"), pybind11::arg("depth"))
+
+            .def("loadCalibration", static_cast<void (PanoWeave::*)(const std::string &)>(&PanoWeave::loadCalibration))
+            .def("setDepth", static_cast<void (PanoWeave::*)(ScalarT)>(&PanoWeave::setDepth))
+            .def("setDepth", static_cast<void (PyPanoWeave::*)(PyCvMat)>(&PyPanoWeave::setDepth))
+
+            .def("resolution", static_cast<std::tuple<int, int> (PyPanoWeave::*)() const>(&PyPanoWeave::resolution))
+            .def("resolution", static_cast<std::tuple<int, int> (PyPanoWeave::*)(const std::tuple<int, int> &)>(&PyPanoWeave::resolution))
+            .def("resolution", static_cast<std::tuple<int, int> (PyPanoWeave::*)(int, int)>(&PyPanoWeave::resolution))
+            .def("width", static_cast<int (PanoWeave::*)() const>(&PanoWeave::width))
+            .def("width", static_cast<int (PanoWeave::*)(int)>(&PanoWeave::width))
+            .def("height", static_cast<int (PanoWeave::*)() const>(&PanoWeave::height))
+            .def("height", static_cast<int (PanoWeave::*)(int)>(&PanoWeave::height))
+
+            .def("fov", static_cast<std::tuple<ScalarT, ScalarT> (PyPanoWeave::*)() const>(&PyPanoWeave::fov))
+            .def("fov", static_cast<std::tuple<ScalarT, ScalarT> (PyPanoWeave::*)(ScalarT, ScalarT)>(&PyPanoWeave::fov))
+            .def("fov", static_cast<std::tuple<ScalarT, ScalarT> (PyPanoWeave::*)(const std::tuple<ScalarT, ScalarT> &)>(&PyPanoWeave::fov))
+            .def("fovX", static_cast<ScalarT (PanoWeave::*)() const>(&PanoWeave::fovX))
+            .def("fovX", static_cast<ScalarT (PanoWeave::*)(ScalarT)>(&PanoWeave::fovX))
+            .def("fovY", static_cast<ScalarT (PanoWeave::*)() const>(&PanoWeave::fovY))
+            .def("fovY", static_cast<ScalarT (PanoWeave::*)(ScalarT)>(&PanoWeave::fovY))
+
+            .def("vignetteThreshold", static_cast<ScalarT (PanoWeave::*)() const>(&PanoWeave::vignetteThreshold))
+            .def("vignetteThreshold", static_cast<ScalarT (PanoWeave::*)(ScalarT)>(&PanoWeave::vignetteThreshold))
+
+            .def("transform", static_cast<ndarray<ScalarT> (PyPanoWeave::*)() const>(&PyPanoWeave::transform))
+            .def("transform", static_cast<ndarray<ScalarT> (PyPanoWeave::*)(const ndarray<ScalarT> &, const std::optional<std::tuple<ScalarT, ScalarT, ScalarT>> &)>(&PyPanoWeave::transform),
+                 pybind11::arg("matrix"), pybind11::arg("translation") = pybind11::none())
+            .def("transform", static_cast<ndarray<ScalarT> (PyPanoWeave::*)(const std::tuple<ScalarT, ScalarT, ScalarT> &, const std::optional<std::tuple<ScalarT, ScalarT, ScalarT>> &)>(&PyPanoWeave::transform),
+                 pybind11::arg("rotation"), pybind11::arg("translation") = pybind11::none())
+
+            .def("stitch", static_cast<PyCvMat (PyPanoWeave::*)(std::vector<PyCvMat> &)>(&PyPanoWeave::weave))
+            .def("stitch", static_cast<PyCvMat (PyPanoWeave::*)(std::vector<PyCvMat> &, ScalarT)>(&PyPanoWeave::weave))
+            .def("stitch", static_cast<PyCvMat (PyPanoWeave::*)(std::vector<PyCvMat> &, PyCvMat)>(&PyPanoWeave::weave));
+    }
+
+}
